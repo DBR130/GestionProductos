1813c266f8267484b8bb3b4e7ca6dbd6d01baee2
DBR130 <bustosrd123@gmail.com>
2026-05-17 13:58:17 -0500

    Agregar archivos de proyecto.

---
+++ GestiónProductos/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestiónProductos.Models;
+
+namespace GestiónProductos.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Productos> Productos { get; set; }
+    
+
+    }
+}
---
+++ GestiónProductos/Models/Productos.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestiónProductos.Models
+{
+    public class Productos
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo nombre es obligatorio")]
+        public string Nombre { get; set; }
+        
+        [Required(ErrorMessage = "El campo categoria es obligatorio")]
+        public string Categoria { get; set; }
+        
+        [Required(ErrorMessage = "El campo precio es obligatorio")]
+        public decimal Precio { get; set; }
+        
+        public bool Disponible { get; set; }
+    }
+}
