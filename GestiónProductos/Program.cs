e39c256661155c4db03dc007cd7c50d050487a70
DBR130 <bustosrd123@gmail.com>
2026-05-17 15:25:41 -0500

    finalización completa del proyecto

--- GestiónProductos/Program.cs
+++ GestiónProductos/Program.cs
@@ -1,3 +1,4 @@
+using GestiónProductos.Data;
 using GestiónProductos.Models;
 using Microsoft.EntityFrameworkCore;
 
@@ -7,7 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<DbContext>(options =>
+builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
